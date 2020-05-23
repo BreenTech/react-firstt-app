d5bc120396c0e80492d47965f0aeaa8b9e3eaa0e
BreenTech <akhtaruzzaman2992@gmail.com>
2020-05-24 01:59:36 +0600

    react practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
-import React from 'react';
+import React  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState ,  useEffect} from 'react';
 
 function App() {
+  
+  const friends =[
+    {name :'sumon', address:'nandipara', age: '23'},
+    {name :'sunny', address:'nandipara', age: '23'},
+    {name :'montu', address:'nandipara', age: '23'},
+    {name :'soikot', address:'nandipara', age: '23'}
+  ]
   return (
+
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Counter></Counter>
+      {
+        friends.map(frnd => <FriendsInfo friend ={frnd}></FriendsInfo>)
+      }
+      <Users></Users>
+      
       </header>
     </div>
   );
 }
+function Users(){
+  const [users, setUsers] =  useState([]);
+  useEffect(() =>{
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data =>setUsers(data));
+  }, [])
+
+  return(
+    <div>
+      <h2>Dynamic User:</h2>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Counter(){
+  
+  const [count, setCount] = useState(0);
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount (count - 1);
+
+  return(
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick ={handleIncrease}>Increase</button>
+      <button onClick = {handleDecrease}>Decrease</button>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function FriendsInfo(props){
+  const friendStyle ={
+    borderRadius : '5px',
+    backgroundColor: 'grey',
+    color : 'yellow',
+    marginTop: '100px'
+}
+  const {name, address, age} = props.friend
+  return(
+    <div style = {friendStyle}>
+      <h1>{name}</h1>
+      <p>{address}</p>
+      <h2>{age}</h2>
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
 
 export default App;
